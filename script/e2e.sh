62bce25f87ad34131398060d9c05f371d7f7658f
Loukas Peteinaris <lpeteinaris@gwi.com>
2025-05-27 23:26:43 +0300

    Fix e2e test code

--- script/e2e.sh
+++ script/e2e.sh
@@ -10,10 +10,8 @@ response_token=$(curl -s --location "$BASE_URL$TOKEN_ENDPOINT" \
   --header 'Content-Type: application/json' \
   --data '{"username": "user", "password": "password"}')
 
-# Extract the token from the response using jq (ensure jq is installed)
 token=$(echo "$response_token" | jq -r '.token')
 
-# Check if the token was successfully retrieved
 if [ -z "$token" ]; then
   echo "Error: Failed to retrieve token."
   exit 1
@@ -24,23 +22,13 @@ echo "----------------------------------"
 
 # Step 2: Create 3 chat sessions for the user and get the first session's ID
 for i in {1..3}; do
-  # Make a POST request to create a chat session
   response=$(curl -s --location --request POST "$BASE_URL/users/$USER_ID/chat-sessions" \
     --header "Authorization: Bearer $token")
 
-  # Extract the chat session ID from the response of the first chat session
   if [ $i -eq 1 ]; then
     CHAT_SESSION_ID=$(echo "$response" | jq -r '.id')
     echo "First chat session created with ID: $CHAT_SESSION_ID"
   fi
-
-  # Check if the session creation was successful
-  #  if [[ $(echo "$response" | jq -r '.status') == "success" ]]; then
-  #    echo "Chat session $i created successfully."
-  #  else
-  #    echo "Error creating chat session $i."
-  #    exit
-  #  fi
 done
 
 # Step 3: Send the first message
@@ -57,7 +45,7 @@ response_message_2=$(curl -s --location "$BASE_URL/users/$USER_ID/chat-sessions/
   --header "Authorization: Bearer $token" \
   --data "{\"content\":\"$message_content_2\"}")
 
-# Step 4.5: Send the third message
+# Step 5: Send the third message
 message_content_3="what social media do they use the most"
 response_message_3=$(curl -s --location "$BASE_URL/users/$USER_ID/chat-sessions/$CHAT_SESSION_ID/messages" \
   --header "Content-Type: application/json" \
@@ -66,25 +54,15 @@ response_message_3=$(curl -s --location "$BASE_URL/users/$USER_ID/chat-sessions/
 
 system_message_id_3=$(echo "$response_message_3" | jq -r '.systemMessage.id')
 
-## Step 5: Display the responses from sending the messages
-#echo "Message 1 sent to chat session $CHAT_SESSION_ID:"
-#echo "$response_message_1"
-#
-#echo "Message 2 sent to chat session $CHAT_SESSION_ID:"
-#echo "$response_message_2"
-#
-#echo "Message 3 sent to chat session $CHAT_SESSION_ID:"
-#echo "$response_message_3"
-
 # Step 6: Fetch and display the whole chat session
 echo "Fetching the entire chat session $CHAT_SESSION_ID..."
 chat_session_response=$(curl -s --location "$BASE_URL/chat-sessions/$CHAT_SESSION_ID" \
   --header "Authorization: Bearer $token")
 
-# Display the full chat session
 echo "Full Chat Session $CHAT_SESSION_ID:"
 echo "$chat_session_response"
 
+# Step 7: Submit feedback for the third message
 feedback_message="The force is not strong with that message."
 feedback_response=$(curl -s --location --write-out "%{http_code}" --request POST "$BASE_URL/users/$USER_ID/chat-sessions/$CHAT_SESSION_ID/messages/$system_message_id_3/feedback" \
   --header "Authorization: Bearer $token" \
@@ -96,3 +74,14 @@ else
   echo "Error: Feedback submission failed. Status code: $feedback_response"
   exit 1
 fi
+
+# Step 8: Send the final and failed message
+message_content_4="what are butterflies"
+response_message_4=$(curl -s --location "$BASE_URL/users/$USER_ID/chat-sessions/$CHAT_SESSION_ID/messages" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer $token" \
+  --data "{\"content\":\"$message_content_4\"}")
+
+
+echo "Message 4 that's supposed to be not answered sent to chat session $CHAT_SESSION_ID:"
+echo "$response_message_4"
